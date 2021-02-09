565fe4452cee146309bdba6b0fed0472385b0a45
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-09 12:31:51 -0300

    Add files via upload

---
+++ Aula 5 Sting.js
@@ -0,0 +1,15 @@
+const curso = "skonks"
+
+console.log(curso.charAt(3))
+console.log(curso.charAt(8)) 
+
+console.log(curso.charCodeAt(3)) // Pega um código em HTML ou ASCII
+console.log(curso.indexOf("k")) // Retorna a letra 
+
+console.log(curso.substring(1))
+console.log(curso.substring(0, 3))
+
+console.log("Curso ".concat(curso).concat("!"))
+console.log(curso.replace("o", "i"))
+
+console.log("Ana, Maria, Carla".split(","))
