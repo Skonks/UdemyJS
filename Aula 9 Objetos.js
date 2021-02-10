694eaa99ca91476da8c52b691e8d7c9f50df31a7
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-10 11:46:41 -0300

    Add files via upload

---
+++ Aula 9 Objetos.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = "skonks daora"
+prod1.preco = 300.00
+
+console.log(prod1)
+
+const prod2 = {
+    nome: "skonks",
+    preco: 100.00,
+    obj: {
+        eita: 100,
+    }
+}
+
+console.log(prod2)
