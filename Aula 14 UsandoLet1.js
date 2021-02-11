dbe03311f62579c0e470247260d348ef950aad20
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-11 10:59:44 -0300

    Delete Aula 14 UsandoLet1.js

--- Aula 14 UsandoLet1.js
+++
@@ -1,6 +0,0 @@
-var numero = 1 //Escopo global e tem escopo de função
-{
-    let numero = 2 //Escopo global, função e escopo de bloco
-    console.log("Dentro", numero)
-}
-console.log("Fora", numero)
