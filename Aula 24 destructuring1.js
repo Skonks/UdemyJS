fa7ffa088ed2cf4699722da5f0ffa11dd9f81d3b
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-12 14:01:48 -0300

    Add files via upload

---
+++ Aula 24 destructuring1.js
@@ -0,0 +1,13 @@
+//"novo" recurso
+
+const pessoa = {
+    nome: "Skonks",
+    idade: 23,
+    endereco: {
+        logradouro: "Rua memes",
+        numero: 1000
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
